31f63f6e3e9bced7a4e7684473cc06946b1b947a
Huanzhi Mao <huanzhimao@gmail.com>
2024-07-28 18:11:34 -0700

    chore: Update utils.py with new functions for loading and writing data

--- berkeley-function-call-leaderboard/bfcl/utils.py
+++ berkeley-function-call-leaderboard/bfcl/utils.py
@@ -1,28 +1,49 @@
-from enum import Enum
+import json
+import os
+from bfcl.model_handler.base import BaseHandler
+from bfcl.model_handler.oss_model import OSS_MODEL_TO_HANDLER_CLS
+from bfcl.model_handler.proprietary_model import PRPPRIETARY_MODEL_TO_HANDLER_CLS
+
+MODEL_TO_HANDLER_CLS = OSS_MODEL_TO_HANDLER_CLS.update(PRPPRIETARY_MODEL_TO_HANDLER_CLS)
 
 
-class CustomEnum(Enum):
-    @classmethod
-    def add(cls, other):
-        combined_members = {member.name: member.value for member in cls}
-        combined_members.update({member.name: member.value for member in other})
-        return __class__(cls.__name__, combined_members)
-    
-    @classmethod
-    def subtract(cls, other):
-        remaining_members = {
-            member.name: member.value 
-            for member in cls if member.value not in other._value2member_map_
-        }
-        return __class__(cls.__name__, remaining_members)
-    
-    @classmethod
-    def rename(cls, new_name):
-        members = {member.name: member.value for member in cls}
-        return __class__(new_name, members)
+def load_model_handler(model_name: str) -> BaseHandler:
+    handler_cls = MODEL_TO_HANDLER_CLS.get(model_name)
+    assert (
+        handler_cls
+    ), f'Invalid model name "{model_name}"! Please select from {tuple(MODEL_TO_HANDLER_CLS)}'
 
-    @classmethod
-    def update(cls, new_members):
-        members = {member.name: member.value for member in cls}
-        members.update(new_members)
-        return __class__(cls.__name__, members)
+    return handler_cls
+
+
+def load_json_file(file_path):
+    result = []
+    with open(file_path) as f:
+        file = f.readlines()
+        for line in file:
+            result.append(json.loads(line))
+    return result
+
+
+def write_list_of_dicts_to_file(
+    data: list, filename: str, subdir=None, appendMode=False
+):
+    if subdir:
+        # Ensure the subdirectory exists
+        os.makedirs(subdir, exist_ok=True)
+
+        # Construct the full path to the file
+        filename = os.path.join(subdir, filename)
+
+    # Write the list of dictionaries to the file in JSON format
+    with open(filename, "a" if appendMode else "w") as f:
+        for i, entry in enumerate(data):
+            json_str = json.dumps(entry)
+            f.write(json_str)
+            # Add a newline when there is just a single entry or not the last entry
+            if i < len(data) - 1 or len(data) == 1:
+                f.write("\n")
+
+
+def write_single_dict_to_file(data: dict, filename: str, subdir=None, appendMode=False):
+    write_list_of_dicts_to_file([data], filename, subdir, appendMode)
